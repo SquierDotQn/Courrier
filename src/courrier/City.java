034263bd69dab370f7b79ab19c4ec94f6ca9aa8b
Théo Plockyn <gamefreak35@hotmail.fr>
2014-11-12 15:36:41 +0100

    Update City.java

--- src/courrier/City.java
+++ src/courrier/City.java
@@ -1,5 +1,43 @@
 package courrier;
 
+import java.util.List;
+
+import lettres.Letter;
+
 public class City {
+
+	private String name;
+	private List<Letter<?>> letterBox;
+	private List<Inhabitant> inhabitants;
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Letter<?>> getLetterBox() {
+		return letterBox;
+	}
+
+	public List<Inhabitant> getInhabitants() {
+		return inhabitants;
+	}
+	
+	public City(String name){
+		this.name = name;
+	}
+	
+	public void sendLetter(Letter<?> letter) {
+		this.letterBox.add(letter);
+	}
+	
+	public void distributeLetter() {
+		for(Letter<?> l : this.letterBox) {
+			l.getDestinataire().receiveLetter(l);
+		}
+	}
+	
+	public void addInhabitant(Inhabitant newcommer) {
+		getInhabitants().add(newcommer);
+	}
 
 }
