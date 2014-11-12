27325380e829ca50b87834aafe15f6f5c6373693
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-12 16:22:03 +0100

    Update Text.java

--- src/courrier/Text.java
+++ src/courrier/Text.java
@@ -1,5 +1,11 @@
 package courrier;
 
+/**
+ * Class for the Text. The money is a String that can be put in a letter.
+ * 
+ * @author Théo Plockyn - Marion Tommasi
+ *
+ */
 public class Text extends AbstractContent<String> {
 
 	public Text(String txt) {
