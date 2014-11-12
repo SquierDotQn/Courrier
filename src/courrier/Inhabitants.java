0f1fd878cc4c558f58717587efeb2afbce44ca2d
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-12 16:21:20 +0100

    Update Inhabitants.java

--- src/courrier/Inhabitants.java
+++ src/courrier/Inhabitants.java
@@ -2,35 +2,74 @@ package courrier;
 
 import lettres.Letter;
 
+/**
+ * Class for the inhabitants. An inhabitant lives in a city and can post and
+ * receive letters.
+ * 
+ * @author Théo Plockyn - Marion Tommasi
+ *
+ */
 public class Inhabitant {
 
 	private String name;
 	private City address;
 	private BankAccount account;
 
-	public Inhabitant(String name, City address, BankAccount account) {
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            the name of the inhabitant
+	 * @param address
+	 *            the city where lives the inhabitant
+	 */
+	public Inhabitant(String name, City address) {
 		this.address = address;
-		this.account = account;
+		this.account = new BankAccount();
 		this.name = name;
 	}
 
+	/**
+	 * 
+	 * @return the name of the inhabitant
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * 
+	 * @return the city where lives the inhabitant
+	 */
 	public City getAdress() {
 		return address;
 	}
 
+	/**
+	 * 
+	 * @return the bank account of the inhabitant
+	 */
 	public BankAccount getAccount() {
 		return account;
 	}
 
+	/**
+	 * Posts a letter.
+	 * 
+	 * @param letter
+	 *            the letter sent
+	 */
 	public void postsLetter(Letter<?> letter) {
 		this.account.debit(letter.getCout());
 		this.address.sendLetter(letter);
 	}
 
+	/**
+	 * Receives a letter.
+	 * 
+	 * @param letter
+	 *            the letter received
+	 */
 	public void receiveLetter(Letter<?> letter) {
 		letter.action();
 	}
