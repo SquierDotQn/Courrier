fb9575b9f45a742cc427994d7b6098cdc7f36c25
Théo Plockyn <gamefreak35@hotmail.fr>
2014-11-12 15:29:20 +0100

    Update AbstractContent.java

--- src/courrier/AbstractContent.java
+++ src/courrier/AbstractContent.java
@@ -1,5 +1,14 @@
 package courrier;
 
-public abstract class AbstractContent implements Content {
+public abstract class AbstractContent<T> implements Content {
 
+	private T value;
+	
+	public AbstractContent (T val) {
+		this.value = val;
+	}
+	
+	public T getValue() {
+		return this.value;
+	}
 }
