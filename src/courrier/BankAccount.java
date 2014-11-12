e1efd953a270529cad9c531378f41272c17d501c
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-12 16:20:03 +0100

    Update BankAccount.java

--- src/courrier/BankAccount.java
+++ src/courrier/BankAccount.java
@@ -1,23 +1,50 @@
 package courrier;
 
+/**
+ * Class for the bank account. A bank account is used to manage the money of the
+ * inhabitants of a city who can post letters.
+ * 
+ * @author Théo Plockyn - Marion Tommasi
+ *
+ */
 public class BankAccount {
 
-	private static final float DEFAULT_ACCOUNT = 0;
-	private float account;
-	
+	private static final double DEFAULT_ACCOUNT = 100;
+	private double account;
+
+	/**
+	 * A bank account is initialized with a certain amount of money (100).
+	 */
 	public BankAccount() {
 		this.account = DEFAULT_ACCOUNT;
 	}
-	
-	public void credit(int sum) {
-		this.account += sum;
+
+	/**
+	 * Adds money to the bank account.
+	 * 
+	 * @param amount
+	 *            the amount of money to add.
+	 */
+	public void credit(int amount) {
+		this.account += amount;
 	}
 
-	public void debit(int sum) {
-		this.account += sum;
+	/**
+	 * Subtracts money to the bank account.
+	 * 
+	 * @param amount
+	 *            the amount of money to substract.
+	 */
+	public void debit(int amount) {
+		this.account += amount;
 	}
-	
-	public float getAccount() {
+
+	/**
+	 * Returns the amount of money in the bank account.
+	 * 
+	 * @return the actual amount of money in the bank account.
+	 */
+	public double getAccount() {
 		return this.account;
 	}
 
