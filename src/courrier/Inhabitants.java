62bfc8b2e7180efa6bd4a98b7aed0967f95d5b2b
Théo Plockyn <gamefreak35@hotmail.fr>
2014-11-12 15:37:29 +0100

    Update Inhabitants.java

--- src/courrier/Inhabitants.java
+++ src/courrier/Inhabitants.java
@@ -1,5 +1,38 @@
 package courrier;
 
-public class Inhabitants {
+import lettres.Letter;
+
+public class Inhabitant {
+
+	private String name;
+	private City address;
+	private BankAccount account;
+
+	public Inhabitant(String name, City address, BankAccount account) {
+		this.address = address;
+		this.account = account;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public City getAdress() {
+		return address;
+	}
+
+	public BankAccount getAccount() {
+		return account;
+	}
+
+	public void postsLetter(Letter<?> letter) {
+		this.account.debit(letter.getCout());
+		this.address.sendLetter(letter);
+	}
+
+	public void receiveLetter(Letter<?> letter) {
+		letter.action();
+	}
 
 }
