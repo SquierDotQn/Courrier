7b200e4c0be52b66b16971846ec0c0da216b036e
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-12 16:21:41 +0100

    Update Money.java

--- src/courrier/Money.java
+++ src/courrier/Money.java
@@ -1,5 +1,11 @@
 package courrier;
 
+/**
+ * Class for the money. The money is a Double that can be send in a letter.
+ * 
+ * @author Théo Plockyn - Marion Tommasi
+ *
+ */
 public class Money extends AbstractContent<Double> {
 
 	public Money(double sum) {
