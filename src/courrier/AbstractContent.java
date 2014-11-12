9af6524d3296270dfb54b47cc4cd74b22ad0bee6
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-12 16:19:36 +0100

    Update AbstractContent.java

--- src/courrier/AbstractContent.java
+++ src/courrier/AbstractContent.java
@@ -1,13 +1,22 @@
 package courrier;
 
+/**
+ * Abstract class for the content of a letter.
+ * 
+ * @author Théo Plockyn - Marion Tommasi
+ *
+ * @param <T>
+ *            any object that we want to put in a letter (String, Float,
+ *            Integer, Double, ...)
+ */
 public abstract class AbstractContent<T> implements Content {
 
 	private T value;
-	
-	public AbstractContent (T val) {
+
+	public AbstractContent(T val) {
 		this.value = val;
 	}
-	
+
 	public T getValue() {
 		return this.value;
 	}
