e8157b09024d4076d8ab45f37271290e2f3c2bef
Théo Plockyn <gamefreak35@hotmail.fr>
2014-11-12 15:30:04 +0100

    Update BankAccount.java

--- src/courrier/BankAccount.java
+++ src/courrier/BankAccount.java
@@ -2,4 +2,23 @@ package courrier;
 
 public class BankAccount {
 
+	private static final float DEFAULT_ACCOUNT = 0;
+	private float account;
+	
+	public BankAccount() {
+		this.account = DEFAULT_ACCOUNT;
+	}
+	
+	public void credit(int sum) {
+		this.account += sum;
+	}
+
+	public void debit(int sum) {
+		this.account += sum;
+	}
+	
+	public float getAccount() {
+		return this.account;
+	}
+
 }
