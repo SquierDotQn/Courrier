116ca840b097c1f49e880acd48a5d82b00791f36
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-11-19 00:10:04 +0100

    Update AbstractLetterDecoratorTest.java

--- test/lettres/AbstractLetterDecoratorTest.java
+++ test/lettres/AbstractLetterDecoratorTest.java
@@ -8,28 +8,7 @@ import static org.junit.Assert.*;
 
 public abstract class AbstractLetterDecoratorTest extends LetterTest {
 	protected Text exampleText = new Text("test");
-	protected Letter<?> exampleLetter = new SimpleLetter(mockIn1,mockIn2, exampleText);
-
-	@Test
-	public void testAction() {
-		// Reinitialize the example
-		exampleLetter = new SimpleLetter(mockIn1,mockIn2, exampleText);
-		assertEquals(exampleText, exampleLetter.content);
-		l.action(); // In the MockUpLetter action, the content is changed
-		assertNotEquals(exampleText, exampleLetter.content);
-	}
-
-}
-
-class MockUpLetter extends SimpleLetter{
-
-	public MockUpLetter(Inhabitant sender, Inhabitant reciever, Text content) {
-		super(sender, reciever, content);
-	}
-	
-	@Override
-	public void action(){
-		content = new Text("mock");
-	}
+	protected Letter<?> exampleLetter = new SimpleLetter(mockIn1, mockIn2,
+			exampleText);
 	
 }
